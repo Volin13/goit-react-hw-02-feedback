4f866e7f3b4771540544ccb7ac12df9058107cf6
Volin13 <110535249+Volin13@users.noreply.github.com>
2022-12-18 20:32:20 +0200

    test2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
-import { Container } from './index';
+import { Container, Section, FeedbackOptions, Statistics } from './index';
 
 export class App extends Component {
-  static defaultProps = {};
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positivePercentage: 0,
+  };
 
-  static propTypes = {};
+  getOptions = () => {
+    return ['good', 'neutral', 'bad'];
+  };
+
+  handleFeedbackBtnClick = ({ target }) => {
+    const { name } = target;
+    const updateState = { ...this.state };
+    updateState[name] += 1;
+    updateState.total = this.countTotalFeedback(updateState);
+    updateState.positivePercentage = this.countPositivePercentage(updateState);
+    this.setState(updateState);
+  };
+
+  countTotalFeedback = ({ good, bad, neutral }) => {
+    console.log(Number(good + bad + neutral));
+    return Number(good + bad + neutral);
+  };
+
+  countPositivePercentage = ({ good, total }) => {
+    console.log(Number((good / total) * 100).toFixed(0));
+    return Number(((good / total) * 100).toFixed(0));
+  };
 
   render() {
-    return <Container>Class Component</Container>;
+    const { good, neutral, bad, total, positivePercentage } = this.state;
+    return (
+      <Container>
+        <Section title="Please leave your feedback">
+          <FeedbackOptions
+            options={this.getOptions()}
+            onLeaveFeedback={this.handleFeedbackBtnClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </Container>
+    );
   }
 }
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,9 +2,10 @@ const Container = ({ children, ...otherProps }) => {
   return (
     <div
       style={{
-        height: '100vh',
+        height: '70vh',
         fontSize: 40,
         color: '#010101',
+        margin: '0 auto',
       }}
       className="container"
       {...otherProps}
